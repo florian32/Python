57b01d00afdcccdbda3695b5c7995877eb084fdf
florian32 <97685387+florian32@users.noreply.github.com>
2022-03-03 13:22:36 +0100

    Add files via upload

---
+++ Simple_Tools/Miles Converter/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+window = Tk()
+window.title("Conveersion")
+window.minsize(width=500, height=500)
+label = Label(text="input miles: ", font=("Arial", 24))
+label.grid(row=0, column=0)
+
+entry = Entry(width=30)
+entry.grid(row=0, column=1)
+
+second_label = Label(text="That is", font=("Arial", 24))
+second_label.grid(row=1, column=0)
+
+third_label = Label(text="0", font=("Arial", 24))
+third_label.grid(row=1, column=1)
+
+miles = Label(text="km", font=("Arial", 24))
+miles.grid(row=1, column=2)
+
+
+def conversion():
+    third_label["text"] = int(entry.get()) * 1.609344
+
+
+button = Button(text="Convert", command=conversion)
+button.grid(row=2, column=1)
+
+window.mainloop()
